e7c96ec0685132553787a8416a408700b3be048a
Divyansh Saxena <divyanshsxn998@gmail.com>
2023-12-03 17:59:55 +0530

    fixed url

--- server/index.js
+++ server/index.js
@@ -22,13 +22,19 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
+// app.use(
+// 	cors({
+// 		origin: "http://localhost:3000",
+// 		credentials: true,
+// 	})
+// )
+
 app.use(
 	cors({
-		origin: ['https://employee-management-application.vercel.app'],
-		methods: ['GET', 'POST', 'PUT', 'DELETE'],
-		crendentials: true
+		origin: "*",
+		credentials: true,
 	})
-)
+);
 
 app.use(
 	fileUpload({
