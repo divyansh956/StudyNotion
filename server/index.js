2154e53bb811dc14d5af6d560a6f337e7485dc95
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2023-12-10 16:58:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,12 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+	cors({
+		origin: "https://studynotion-frontend-mu.vercel.app",
+		credentials: true,
+	})
+);
 
 app.use(
 	fileUpload({
